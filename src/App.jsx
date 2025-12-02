3b6a9082d149ee678d671d938d0a06347d2adbfd
“midory-rojas” <midory.rojasv@gmail.com>
2025-12-02 16:03:38 +0100

    feat:Stampo dinamica la lista di genere dei film

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
 
 import './App.css'
 
+const filmsArray = [
+  { title: 'Inception', genere: 'Fantascienza' },
+  { title: 'Il Padrino', genere: 'Thriller' },
+  { title: 'Titanic', genere: 'Romantico' },
+  { title: 'Batman', genere: 'Azione' },
+  { title: 'Interstellar', genere: 'Fantascienza' },
+  { title: 'Pulp Fiction', genere: 'Thriller' },
+];
+
 function App() {
 
 
   return (
     <>
+      <div className='container py-4'>
+        <header>
+          <h1>Elenchi di Film</h1>
+        </header>
+        <main className='py-4'>
+          <select className="form-select">
+            <option selected>Scegli il genere del film</option>
+            {filmsArray.map((film, index) => (
+              <option key={index} className='list-group-item'>
+                {film.genere}
+              </option>
+            ))}
+        </select>
+      </main >
+    </div >
     </>
   )
 }
